77a42619c4bee962184876fa0e210387bebb6b44
angelinarair33 <107152440+angelinarair33@users.noreply.github.com>
2022-07-02 19:09:16 -0400

    I added a feature that causes the page to redirect to a form when the timer ends.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,17 @@
 document.getElementById("scores").onclick = function () {
  location.href = '/Code-Quiz-M4/indexscores.html';
 }
+
+document.getElementById("button").onclick = function () {
+    const element = document.getElementById("timer");
+    var time = 121;
+    setInterval(function() {
+        time--
+        element.innerHTML = time
+        if (time === 0) {
+            setTimeout(function(){
+                window.location.href = '/Code-Quiz-M4/indexform.html';
+             }, );
+        }
+    }, 1000);
+}
